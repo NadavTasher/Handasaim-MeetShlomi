afb73e941b8444f67fa2d6c0b15d9323530d863b
nadav <tashernadav@gmail.com>
2018-12-08 17:44:15 +0200

    Changes

--- admin/javascript/app.js
+++ admin/javascript/app.js
@@ -1,2 +1,131 @@
 function load() {
+    hideAll();
+    if (getCookie("Admin") !== undefined && getCookie("Admin") !== "undefined") {
+        home();
+    } else {
+        login();
+    }
+}
+
+function home() {
+    hideAll();
+    hideHome();
+    show("home");
+    show("menu");
+}
+
+function pending() {
+    hideAll();
+    hideHome();
+    show("home");
+    show("pending");
+    loadPending(getCookie("Admin"), (json) => {
+        let pending = get("pending");
+        clear(pending);
+        let meetings = json.results;
+        for (let m=0;m<meetings.length;m++){
+            let currentMeeting = meetings[m];
+            let meeting = document.createElement("div");
+            let bottom = document.createElement("div");
+            let choosing=document.createElement("div");
+            let title = document.createElement("p");
+            let date = document.createElement("p");
+            let time = document.createElement("p");
+            let approve = document.createElement("img");
+            let decline = document.createElement("img");
+            meeting.classList.add("meeting");
+            title.innerHTML = currentMeeting.content.reason;
+            date.innerHTML = (currentMeeting.slot.date.day) + "." + (currentMeeting.slot.date.month);
+            let dayMinutes = currentMeeting.slot.slot * json.slot;
+            let minutes = dayMinutes % 60;
+            let hours = (dayMinutes - minutes) / 60;
+            time.innerHTML = hours + ":" + (minutes < 10 ? "0" : "") + minutes;
+
+            approve.src="images/approved.svg";
+            decline.src="images/denied.svg";
+
+            choosing.appendChild(approve);
+            choosing.appendChild(decline);
+
+            meeting.appendChild(title);
+            bottom.appendChild(date);
+            bottom.appendChild(time);
+            bottom.appendChild(choosing);
+            meeting.appendChild(bottom);
+            pending.appendChild(meeting);
+        }
+    });
+}
+
+function calendar() {
+    hideAll();
+    hideHome();
+    show("home");
+    show("calendar");
+}
+
+function login() {
+    hideAll();
+    show("login");
+}
+
+function submitLogin() {
+    let password = get("login-password");
+    if (password.value.length > 0) {
+        loadLogin(password.value, (json) => {
+            if (json.auth) {
+                setCookie("Admin", password.value);
+            }
+            refresh();
+        });
+    } else {
+        alert("Password must be filled");
+    }
+}
+
+function loadLogin(password, callback) {
+    let body = new FormData;
+    body.append("key", password);
+    fetch("php/admin.php", {
+        method: "POST",
+        cache: "no-store",
+        headers: {
+            'Cache-Control': 'no-cache'
+        },
+        body: body
+    }).then(response => {
+        response.text().then((response) => {
+            callback(JSON.parse(response));
+        });
+    });
+}
+
+function loadPending(password, callback) {
+    let body = new FormData;
+    body.append("key", password);
+    body.append("action", "get");
+    body.append("get", "pending");
+    fetch("php/admin.php", {
+        method: "POST",
+        cache: "no-store",
+        headers: {
+            'Cache-Control': 'no-cache'
+        },
+        body: body
+    }).then(response => {
+        response.text().then((response) => {
+            callback(JSON.parse(response));
+        });
+    });
+}
+
+function hideAll() {
+    hide("login");
+    hide("home");
+}
+
+function hideHome() {
+    hide("pending");
+    hide("calendar");
+    hide("menu");
 }
--- admin/php/admin.php
+++ admin/php/admin.php
@@ -1,7 +1,80 @@
 <?php
-/**
- * Created by IntelliJ IDEA.
- * User: nadav
- * Date: 12/8/18
- * Time: 4:27 PM
- */
+// Init Vars
+$result = new stdClass();
+$settings = json_decode(file_get_contents('../files/settings.json'));
+$msprefs = json_decode(file_get_contents('../../files/settings.json'));
+$dbFile = '../../files/database.json';
+$db = json_decode(file_get_contents($dbFile));
+// Main
+main();
+// Functions
+function main()
+{
+    global $result, $msprefs;
+    if (isset($_POST["key"])) {
+        // User Actions
+        $key = $_POST["key"];
+        $auth = checkKey($key);
+        $result->auth = $auth;
+        if ($auth) {
+            if (isset($_POST["action"])) {
+                $action = $_POST["action"];
+                if ($action === "get") {
+                    if (isset($_POST["get"])) {
+                        $get = $_POST["get"];
+                        if ($get === "pending" || $get === "approved" || $get === "denied") {
+                            $result->results = byState($get);
+                        } else if ($get === "date") {
+                            if (isset($_POST["date"])) {
+                                $date = json_decode($_POST["date"]);
+                                $result->results = byDate($date);
+                            }
+                        }
+                    }
+                    if (!isset($result->results)) {
+                        $result->results = array();
+                    }
+                    $result->slot = $msprefs->slot;
+                } else if ($action === "set") {
+
+                } else {
+                    $result->error = "Unknown Action";
+                }
+            }
+        }
+    } else {
+        $result->error = "No Key";
+    }
+    echo json_encode($result);
+}
+
+function byState($get)
+{
+    global $db;
+    $result = array();
+    $meetings = $db->meetings;
+    for ($m = 0; $m < sizeof($meetings); $m++) {
+        if ($meetings[$m]->state === $get) array_push($result, $meetings[$m]);
+    }
+    return $result;
+}
+
+function byDate($date)
+{
+    global $db;
+    $result = array();
+
+    return $result;
+}
+
+function checkKey($key)
+{
+    global $settings;
+    return $key === $settings->password;
+}
+
+function save()
+{
+    global $db, $dbFile;
+    file_put_contents($dbFile, json_encode($db));
+}
